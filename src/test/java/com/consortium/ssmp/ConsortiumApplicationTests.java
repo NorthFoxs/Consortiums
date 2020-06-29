936245e45ae1d1f3eac8ab036fe03c08b6a092af
NorthFoxs <1113921702@qq.com>
2020-06-29 10:07:29 +0800

    Initial Commit

---
+++ src/test/java/com/consortium/ssmp/ConsortiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consortium.ssmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsortiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
